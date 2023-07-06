03760c5b47e66afd56e07938e7744e1830f49d72
phileurobasic02 <138648330+phileurobasic02@users.noreply.github.com>
2023-07-06 08:11:59 +0200

    Create string_operations.py

---
+++ string_operations.py
@@ -0,0 +1,9 @@
+from transformations import capitalize, reverse
+
+text = "hello, world!"
+
+capitalized_text = capitalize(text)
+print(f"Capitalized text: {capitalized_text}")
+
+reversed_text = reverse(text)
+print(f"Reversed text: {reversed_text}")
